6ebd5e6425b2aa8ffc28f888f7db2ef2cd5ea02f
LeoFeitosa <ffeitosa.leonardo@gmail.com>
2015-05-01 20:48:47 -0300

    CodeIgniter - Data por Extenso
    Chamar o metodo escrever_data(); e passar o datetime no formato 'Y-m-d
    H:i:s'

---
+++ dataextenso_helper.php
@@ -0,0 +1,44 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+if ( ! function_exists('escrever_data'))
+{
+    function escrever_data($datetime) {
+        
+        $data = explode("-", $datetime);
+        $dianum = explode(" ", $data[2]);
+        $dia = $dianum[0];
+        $mes = date($data[1]);
+        $ano = date($data[0]);
+        
+        $semana = date('w', mktime(0,0,0,$mes,$dia,$ano) / 7 ) + 1;
+
+        // configuração mes
+        switch ($mes){
+        case 1: $mes = "Janeiro"; break;
+        case 2: $mes = "Fevereiro"; break;
+        case 3: $mes = "Março"; break;
+        case 4: $mes = "Abril"; break;
+        case 5: $mes = "Maio"; break;
+        case 6: $mes = "Junho"; break;
+        case 7: $mes = "Julho"; break;
+        case 8: $mes = "Agosto"; break;
+        case 9: $mes = "Setembro"; break;
+        case 10: $mes = "Outubro"; break;
+        case 11: $mes = "Novembro"; break;
+        case 12: $mes = "Dezembro"; break;
+        }
+
+        // configuração semana
+        switch ($semana) {
+        case 0: $semana = "Domingo"; break;
+        case 1: $semana = "Segunda Feira"; break;
+        case 2: $semana = "Terça Feira"; break;
+        case 3: $semana = "Quarta Feira"; break;
+        case 4: $semana = "Quinta Feira"; break;
+        case 5: $semana = "Sexta Feira"; break;
+        case 6: $semana = "Sábado"; break;
+        }
+        //Agora basta imprimir na tela...
+        return ("$semana, $dia de $mes de $ano");
+    }
+}
